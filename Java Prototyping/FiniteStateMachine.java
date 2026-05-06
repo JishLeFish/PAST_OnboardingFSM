95d64f6746ae2e945d542a996d530952f1e4d41b
Joshua Norton <jcnorton1357@gmail.com>
2026-05-06 12:43:47 +0800

    add getUserInput Method, and create stateBoot Method

--- Java Prototyping/FiniteStateMachine.java
+++ Java Prototyping/FiniteStateMachine.java
@@ -1,5 +1,32 @@
 import java.util.*;
+public class FiniteStateMachine 
+{
+    static Scanner sc = new Scanner(System.in);
 
-public class FiniteStateMachine {
-    
+    public static void main(String[] args)
+    {
+        stateBoot();
+    }
+
+    public static String getUserInput(String prompt)
+    {
+        String input;
+        try
+        {
+            System.out.println(prompt);
+            input = sc.next();
+        }
+        catch(InputMismatchException e)
+        {
+            System.out.println("There was an error with getting the User Input.");
+            System.out.println("The error: " + e);
+        }
+
+        return input;
+    }
+    public static void stateBoot()
+    {
+        System.out.println("The System is booting.");
+
+    }
 }
